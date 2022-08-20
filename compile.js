8cf63c8799d5a7425a3459bb8b25b0314f95d683
Akshat0102 <76106653+Akshat0102@users.noreply.github.com>
2022-08-20 12:01:16 +0530

    updated deploy and compile js files

--- compile.js
+++ compile.js
@@ -1,8 +1,8 @@
-path = require('path');
-fs = require('fs');
-solc = require('solc');
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
 
-lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
-source = fs.readFileSync(lotteryPath, 'utf-8');
+const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
+const source = fs.readFileSync(lotteryPath, 'utf8');
 
-module.exports = solc.compile(source, 1).contracts[':Lottery']
+module.exports = solc.compile(source, 1).contracts[':Lottery'];
