ab66bd49ae6f9e2a770dde90150e73d88146f57b
Akshat0102 <76106653+Akshat0102@users.noreply.github.com>
2022-08-21 14:10:27 +0530

    updated contract and solc version

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.4.16 <0.9.0;
+pragma solidity >=0.5.0 <0.9.0;
 
 contract Lottery{
+
+    string internal constant INSUFFICIENT_FUND_MESSAGE = "Insufficient Funds";
+    string internal constant RESTRICTED_MESSAGE = "Unauthorized Access";
+
     address public manager;
-    address[] public players;
+    address payable[] public players;
 
     constructor() {
         manager = msg.sender;
@@ -11,33 +15,33 @@ contract Lottery{
 
     function enterLottery() public payable{
         //players need to atleast pitch in 0.1 ether to enter the tournament
-        require(msg.value > 0.1 ether);
-        players.push(msg.sender);
+        require(msg.value > 0.1 ether, INSUFFICIENT_FUND_MESSAGE);
+        players.push(payable(msg.sender));
     }
 
     function randomGen() private view returns(uint){
         return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
     }
 
-    function pickWinner() public restricted{
+    function pickWinner() public payable restricted{
         //getting pseudo-random index with randomGen()
         uint idx = randomGen() % players.length;
 
         //transfering lottery amount to winner
-        payable(players[idx]).transfer(address(this).balance);  
+        players[idx].transfer(address(this).balance);
 
         //reseting the players to 0 
-        players = new address[](0);
+        players = new address payable[](0);
     }
 
     //making a restricted modifier allowing only manager to pick Winner
     modifier restricted(){
-        require(msg.sender==manager);
+        require(msg.sender==manager, RESTRICTED_MESSAGE);
         _;
     }
 
     //function to get the addresses of all the players entered
-    function getPlayers() public view returns(address[] memory) {
+    function getPlayers() public view returns(address payable[] memory) {
         return players;
     }
 }
